c289819e0804227a815aa09560f9def6c71b1e11
alex.dd84 <thibujixo>
2019-07-28 18:45:31 +0300

    init

--- Dockerfile
+++ Dockerfile
@@ -2,3 +2,17 @@ FROM nvcr.io/nvidia/pytorch:19.05-py3
 
 COPY . retinanet/
 RUN pip install --no-cache-dir -e retinanet/
+
+RUN apt-get update && apt-get install -y openssh-server
+RUN mkdir /var/run/sshd
+RUN echo 'root:pass' | chpasswd
+RUN sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+
+# SSH login fix. Otherwise user is kicked off after login
+RUN sed 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g' -i /etc/pam.d/sshd
+
+ENV NOTVISIBLE "in users profile"
+RUN echo "export VISIBLE=now" >> /etc/profile
+
+EXPOSE 22
+CMD ["/usr/sbin/sshd", "-D"]
--- retinanet/train.py
+++ retinanet/train.py
@@ -1,19 +1,22 @@
+from math import isfinite
 from statistics import mean
-from math import isfinite 
+
 import torch
-from torch.optim import SGD
-from torch.optim.lr_scheduler import LambdaLR
-from torch.utils.data import DataLoader
-from apex import amp, optimizers
+from apex import amp
 from apex.parallel import DistributedDataParallel
+from torch.optim import Adam
+from torch.optim.lr_scheduler import ReduceLROnPlateau, LambdaLR
+
 from .backbones.layers import convert_fixedbn_model
-
-from .data import DataIterator
 from .dali import DaliDataIterator
-from .utils import ignore_sigint, post_metrics, Profiler
+from .data import DataIterator
 from .infer import infer
+from .utils import ignore_sigint, post_metrics, Profiler
 
-def train(model, state, path, annotations, val_path, val_annotations, resize, max_size, jitter, batch_size, iterations, val_iterations, mixed_precision, lr, warmup, milestones, gamma, is_master=True, world=1, use_dali=True, verbose=True, metrics_url=None, logdir=None):
+
+def train(model, state, path, annotations, val_path, val_annotations, resize, max_size, jitter, batch_size, iterations,
+          val_iterations, mixed_precision, lr, warmup, milestones, gamma, is_master=True, world=1, use_dali=True,
+          verbose=True, metrics_url=None, logdir=None):
     'Train the model on the given dataset'
 
     # Prepare model
@@ -25,26 +28,31 @@ def train(model, state, path, annotations, val_path, val_annotations, resize, ma
         model = model.cuda()
 
     # Setup optimizer and schedule
-    optimizer = SGD(model.parameters(), lr=lr, weight_decay=0.0001, momentum=0.9) 
+    # optimizer = SGD(model.parameters(), lr=lr, weight_decay=0.0001, momentum=0.9)
+    optimizer = Adam(model.parameters(), lr=lr, weight_decay=0.0000001)
 
     model, optimizer = amp.initialize(model, optimizer,
-                                      opt_level = 'O2' if mixed_precision else 'O0',
-                                      keep_batchnorm_fp32 = True,
-                                      loss_scale = 128.0,
-                                      verbosity = is_master)
+                                      opt_level='O2' if mixed_precision else 'O0',
+                                      keep_batchnorm_fp32=True,
+                                      loss_scale=128.0,
+                                      verbosity=is_master)
 
-    if world > 1: 
+    if world > 1:
         model = DistributedDataParallel(model)
     model.train()
 
     if 'optimizer' in state:
         optimizer.load_state_dict(state['optimizer'])
 
+    #'''
     def schedule(train_iter):
         if warmup and train_iter <= warmup:
             return 0.9 * train_iter / warmup + 0.1
         return gamma ** len([m for m in milestones if m <= train_iter])
     scheduler = LambdaLR(optimizer, schedule)
+    #'''
+
+    #scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.5, patience=2, verbose=True)
 
     # Prepare dataset
     if verbose: print('Preparing dataset...')
@@ -52,7 +60,6 @@ def train(model, state, path, annotations, val_path, val_annotations, resize, ma
         path, jitter, max_size, batch_size, stride,
         world, annotations, training=True)
     if verbose: print(data_iterator)
-
 
     if verbose:
         print('    device: {} {}'.format(
@@ -113,7 +120,7 @@ def train(model, state, path, annotations, val_path, val_annotations, resize, ma
                 box_loss = torch.stack(list(box_losses)).mean().item()
                 learning_rate = optimizer.param_groups[0]['lr']
                 if verbose:
-                    msg  = '[{:{len}}/{}]'.format(iteration, iterations, len=len(str(iterations)))
+                    msg = '[{:{len}}/{}]'.format(iteration, iterations, len=len(str(iterations)))
                     msg += ' focal loss: {:.3f}'.format(focal_loss)
                     msg += ', box loss: {:.3f}'.format(box_loss)
                     msg += ', {:.3f}s/{}-batch'.format(profiler.means['train'], batch_size)
@@ -123,7 +130,7 @@ def train(model, state, path, annotations, val_path, val_annotations, resize, ma
                     print(msg, flush=True)
 
                 if logdir is not None:
-                    writer.add_scalar('focal_loss', focal_loss,  iteration)
+                    writer.add_scalar('focal_loss', focal_loss, iteration)
                     writer.add_scalar('box_loss', box_loss, iteration)
                     writer.add_scalar('learning_rate', learning_rate, iteration)
                     del box_loss, focal_loss
@@ -150,7 +157,10 @@ def train(model, state, path, annotations, val_path, val_annotations, resize, ma
 
             if val_annotations and (iteration == iterations or iteration % val_iterations == 0):
                 infer(model, val_path, None, resize, max_size, batch_size, annotations=val_annotations,
-                    mixed_precision=mixed_precision, is_master=is_master, world=world, use_dali=use_dali, is_validation=True, verbose=False)
+                             mixed_precision=mixed_precision, is_master=is_master, world=world, use_dali=use_dali,
+                             is_validation=True, verbose=False)
+                #if not isinstance(loss, str):
+                    #scheduler.step(loss)
                 model.train()
 
             if iteration == iterations:
@@ -158,5 +168,3 @@ def train(model, state, path, annotations, val_path, val_annotations, resize, ma
 
     if logdir is not None:
         writer.close()
-
-
